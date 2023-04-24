b813f8237852f01ee513c392bafe16f3d5a70ff8
willfranklineller <122483522+willfranklineller@users.noreply.github.com>
2023-04-24 17:41:06 -0400

    Added additional mental-health motivators

--- motivators.py
+++ motivators.py
@@ -7,4 +7,10 @@ motivators = [
 	"You’re not alone in this",
 	"Do you want a hug?",
 	"We can beat this together!",
+	"You have so much strength inside of you.",
+	"Don't give up!",
+	"Hope is worth the risk.",
+	"Remember: you've made it through every storm you've ever faced.",
+	"Take a deep breath in and out.",
+	"Don't worry too much on the outcome; what's your next right thing?"
 ]
